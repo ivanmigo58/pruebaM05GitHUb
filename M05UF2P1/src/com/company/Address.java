6cd989489e1d0e7cfb94d14a0f6a272c5996e372
ivanmigo58 <ivanmiralles58@gmail.com>
2021-03-08 16:52:20 +0100

    Probando Maven

--- M05UF2P1/src/com/company/Address.java
+++ M05UF2P1/src/com/company/Address.java
@@ -1,2 +1,9 @@
-package com.company;public class Address {
+package com.company;
+
+public class Address implements Cloneable
+{
+    public String street;
+    public String city;
+    public String state;
+    public String zipCode;
 }
